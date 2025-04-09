2e38443ad2e6f4e6556ca81376b3bf8f3e7538b7
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-04-09 15:45:15 +0100

    shadow not implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
 
 	shadow(libs.bundles.qupath)
 	shadow(libs.bundles.logging)
-	implementation(libs.extensionmanager)
+	shadow(libs.extensionmanager)
 
 	// For testing
 	testImplementation(libs.bundles.qupath)
